a668fb640e888aedd0e25571f20b10d0bc98a87a
Bill Chan <biaocy91@gmail.com>
2018-12-12 00:27:06 +0800

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -23,7 +23,7 @@ EOF
 # 0: Patch
 IS_PATCH=2
 
-# First argument is operation [patch|undo]
+# First argument is operation 
 OP="${1}"
 
 if [ -z "$OP" ]
@@ -52,84 +52,149 @@ IRONIC_DIR="$PY_DIST_DIR/ironic"
 # Ironic egg info dir
 IRONIC_EGG_DIR="$PY_DIST_DIR/ironic-$IRONIC_VERSION.egg-info"
 
+# Create temporary dir, all original file copy in this dir, before modification.
+# These files IS for reference only, the script will not recover from these copy
+# directly...
+TMP_DIR="$PWD/tmp"
+mkdir -p $TMP_DIR
+
 # Files and copys path
-BAK_SUFFIX='.bak'
+BAK_SUFFIX=".$(date +%Y%m%d-%H%M%S)"
 PATH_CONF_INIT="$IRONIC_DIR/conf/__init__.py"
-PATH_CONF_INIT_BAK="$IRONIC_DIR/conf/__init__.py$BAK_SUFFIX"
+PATH_CONF_INIT_BAK="$TMP_DIR/__init__.py$BAK_SUFFIX"
 PATH_COMMON_EXCEPTION="$IRONIC_DIR/common/exception.py"
-PATH_COMMON_EXCEPTION_BAK="$IRONIC_DIR/common/exception.py$BAK_SUFFIX"
+PATH_COMMON_EXCEPTION_BAK="$TMP_DIR/exception.py$BAK_SUFFIX"
 PATH_ENTRY_POINTS="$IRONIC_EGG_DIR/entry_points.txt"
-PATH_ENTRY_POINTS_BAK="$IRONIC_EGG_DIR/entry_points.txt$BAK_SUFFIX"
+PATH_ENTRY_POINTS_BAK="$TMP_DIR/entry_points.txt$BAK_SUFFIX"
 PATH_IRONIC_CONF="/etc/ironic/ironic.conf"
-PATH_IRONIC_CONF_BAK="${PATH_IRONIC_CONF}${BAK_SUFFIX}"
+PATH_IRONIC_CONF_BAK="$TMP_DIR/ironic.conf${BAK_SUFFIX}"
 # Patch file
 PATCH_FILE="$PWD/ironic_driver_for_iBMC.tar"
+# Patch code...
+CODE_CONF_INIT='from ironic.conf import ibmc\nibmc.register_opts(CONF)\n\n'
+CODE_COMMON_EXCEPTION='class IBMCError(IronicException):\n    _msg_fmt = _("IBMC exception occurred. Error: %(error)s")\n\n\nclass IBMCConnectionError(IBMCError):\n    _msg_fmt = _("IBMC connection failed for node %(node)s: %(error)s")\n\n'
+# Patch entry points ...
+INTERFACE_MGMT='ironic.hardware.interfaces.management'
+INTERFACE_POWER='ironic.hardware.interfaces.power'
+INTERFACE_VENDOR='ironic.hardware.interfaces.vendor'
+HARDWARE_TYPES='ironic.hardware.types'
+IBMC_MGMT='ibmc = ironic.drivers.modules.ibmc.management:IBMCManagement'
+IBMC_POWER='ibmc = ironic.drivers.modules.ibmc.power:IBMCPower'
+IBMC_VENDOR='ibmc = ironic.drivers.modules.ibmc.vendor:IBMCVendor'
+IBMC_HW='ibmc = ironic.drivers.ibmc:IBMCHardware'
+# Patch config ...
+IRONIC_CFG_HW="enabled_hardware_types"
+IRONIC_CFG_MGMT="enabled_management_interfaces"
+IRONIC_CFG_POWER="enabled_power_interfaces"
+IRONIC_CFG_VENDOR="enabled_vendor_interfaces"
+
+# Check environment correctness
+function check_envir {
+    # Check files, which need modification, whether in consistent state
+    # Code ...
+    local EX_CONF_INIT=$(grep -E "^from ironic.conf import ibmc" $PATH_CONF_INIT || :)
+    local EX_COMMON_EXCEPTION=$(grep -E "^class IBMCError" $PATH_COMMON_EXCEPTION || :)
+    # Entry points ...
+    local EX_EP_IBMC_HW=$(grep -E "^$IBMC_HW" $PATH_ENTRY_POINTS || :)
+    local EX_EP_IBMC_MGMT=$(grep -E "^$IBMC_MGMT" $PATH_ENTRY_POINTS || :)
+    local EX_EP_IBMC_POWER=$(grep -E "^$IBMC_POWER" $PATH_ENTRY_POINTS || :)
+    local EX_EP_IBMC_VENDOR=$(grep -E "^$IBMC_VENDOR" $PATH_ENTRY_POINTS || :)
+    # Config ...
+    local EX_CFG_IBMC_HW=$(grep -E "^$IRONIC_CFG_HW=.*ibmc.*" $PATH_IRONIC_CONF || :)
+    local EX_CFG_IBMC_MGMT=$(grep -E "^$IRONIC_CFG_MGMT=.*ibmc.*" $PATH_IRONIC_CONF || :)
+    local EX_CFG_IBMC_POWER=$(grep -E "^$IRONIC_CFG_POWER=.*ibmc.*" $PATH_IRONIC_CONF || :)
+    local EX_CFG_IBMC_VENDOR=$(grep -E "^$IRONIC_CFG_VENDOR=.*ibmc.*" $PATH_IRONIC_CONF || :)
+
+    # Files that are not in consistent state
+    local EX_FILES
+    if [ ! -z "$EX_CONF_INIT" ]
+    then
+        EX_FILES[${#EX_FILES[@]}]='\n'
+        EX_FILES[${#EX_FILES[@]}]=$PATH_CONF_INIT
+    fi
+    if [ ! -z "$EX_COMMON_EXCEPTION" ]
+    then
+        EX_FILES[${#EX_FILES[@]}]='\n'
+        EX_FILES[${#EX_FILES[@]}]=$PATH_COMMON_EXCEPTION
+    fi
+    if [ ! -z "$EX_EP_IBMC_HW" -o ! -z "$EX_EP_IBMC_POWER" -o \
+         ! -z "$EX_EP_IBMC_VENDOR" -o ! -z "$EX_EP_IBMC_MGMT" ]
+    then
+        EX_FILES[${#EX_FILES[@]}]='\n'
+        EX_FILES[${#EX_FILES[@]}]=$PATH_ENTRY_POINTS
+    fi
+    if [ ! -z "$EX_CFG_IBMC_HW" -o ! -z "$EX_CFG_IBMC_MGMT" -o \
+         ! -z "$EX_CFG_IBMC_POWER" -o ! -z "$EX_CFG_IBMC_VENDOR" ]
+    then
+        EX_FILES[${#EX_FILES[@]}]='\n'
+        EX_FILES[${#EX_FILES[@]}]=$PATH_IRONIC_CONF
+    fi
+
+    if [ ! "${#EX_FILES[@]}" -gt "0" ]
+    then
+        return 0
+    else
+        echo -e "Patch failed!
+
+Following files may have iBMC related configuration or codes. 
+You could uninstall this script to erase iBMC related configuration or codes,
+or check aginst these files then update manually.
+        ${EX_FILES[@]}
+        "
+        exit 1
+    fi
+}
 
 # Replace or append ibmc related config
 function config {
     local OPTION_STR="$1"
 
     ENABLED_STR=$(grep -E "^$OPTION_STR=" $PATH_IRONIC_CONF || :)
-    ENABLED_STR_NOT_EMPTY=$(grep -E "^$OPTION_STR=(.+)" $PATH_IRONIC_CONF || :)
     IBMC_TYPE="ibmc"
     ENABLED=$(echo $ENABLED_STR | grep -E "ibmc" || :)
-    if [ ! -z ENABLED ]
+    if [ ! -z "$ENABLED" ]
     then
         return # Already enabled ibmc related config
     fi
 
-    if [ ! -z "$ENABLED_STR" -a ! -z "$ENABLED_STR_NOT_EMPTY" ]
+    if [ ! -z "$ENABLED_STR" ]
     then
-        sed -i$BAK_SUFFIX -r -e "s/$ENABLED_STR/${ENABLED_STR},${IBMC_TYPE}/" $PATH_IRONIC_CONF
-    elif [ ! -z "$ENABLED_STR" -a -z "$ENABLED_STR_NOT_EMPTY" ]
-    then
-        sed -i$BAK_SUFFIX -r -e "s/$ENABLED_STR/${ENABLED_STR}${IBMC_TYPE}/" $PATH_IRONIC_CONF
+        sed -i -r -e "s/$ENABLED_STR/${ENABLED_STR},${IBMC_TYPE}/" $PATH_IRONIC_CONF
     else
+        # option not exist
         echo "$OPTION_STR=$IBMC_TYPE" >> $PATH_IRONIC_CONF
     fi
 }
 
+function make_copy {
+    # Make copy, for reference only...
+    cp $PATH_CONF_INIT $PATH_CONF_INIT_BAK
+    cp $PATH_COMMON_EXCEPTION $PATH_COMMON_EXCEPTION_BAK
+    cp $PATH_IRONIC_CONF $PATH_IRONIC_CONF_BAK
+    cp $PATH_ENTRY_POINTS $PATH_ENTRY_POINTS_BAK
+}
+
 # Patch function 
 function patch {
+    # Check environment first, ensure the environment is iBMC not related
+    check_envir
+
+    make_copy
+
     # Extract added patch files to Python dist-packages
     tar -xf $PATCH_FILE -C $PY_DIST_DIR
 
-    # Make copy, in case something broken after this script run,
-    #  we can restore these file to recover ironic service
-    cp $PATH_CONF_INIT $PATH_CONF_INIT_BAK
-    cp $PATH_COMMON_EXCEPTION $PATH_COMMON_EXCEPTION_BAK
-
     # Add iBMC related conf
-    echo '
-
-from ironic.conf import ibmc
-ibmc.register_opts(CONF)
-' >> $PATH_CONF_INIT
+    printf "\n%b" "$CODE_CONF_INIT" >> $PATH_CONF_INIT
 
     # Add iBMC related exceptions
-    echo '
-
-class IBMCError(IronicException):
-    _msg_fmt = _("IBMC exception occurred. Error: %(error)s")
-
-
-class IBMCConnectionError(IBMCError):
-    _msg_fmt = _("IBMC connection failed for node %(node)s: %(error)s")
-' >> $PATH_COMMON_EXCEPTION
+    printf "\n%b" "$CODE_COMMON_EXCEPTION" >> $PATH_COMMON_EXCEPTION
 
     # Modify ironic egg info entry_points.txt
-    INTERFACE_MGMT='ironic.hardware.interfaces.management'
-    INTERFACE_POWER='ironic.hardware.interfaces.power'
-    INTERFACE_VENDOR='ironic.hardware.interfaces.vendor'
-    HARDWARE_TYPES='ironic.hardware.types'
-    IBMC_MGMT='ibmc = ironic.drivers.modules.ibmc.management:IBMCManagement'
-    IBMC_POWER='ibmc = ironic.drivers.modules.ibmc.power:IBMCPower'
-    IBMC_VENDOR='ibmc = ironic.drivers.modules.ibmc.vendor:IBMCVendor'
-    IBMC_HW_TYPE='ibmc = ironic.drivers.ibmc:IBMCHardware'
-    sed -i$BAK_SUFFIX -r -e "s/(\[$INTERFACE_MGMT\])/\1\n$IBMC_MGMT/" $PATH_ENTRY_POINTS
+    sed -i -r -e "s/(\[$INTERFACE_MGMT\])/\1\n$IBMC_MGMT/" $PATH_ENTRY_POINTS
     sed -i -r -e "s/(\[$INTERFACE_POWER\])/\1\n$IBMC_POWER/" $PATH_ENTRY_POINTS
     sed -i -r -e "s/(\[$INTERFACE_VENDOR\])/\1\n$IBMC_VENDOR/" $PATH_ENTRY_POINTS
-    sed -i -r -e "s/(\[$HARDWARE_TYPES\])/\1\n$IBMC_HW_TYPE/" $PATH_ENTRY_POINTS
+    sed -i -r -e "s/(\[$HARDWARE_TYPES\])/\1\n$IBMC_HW/" $PATH_ENTRY_POINTS
 
     # Enabled ibmc related config
     config "enabled_hardware_types"
@@ -142,24 +207,32 @@ class IBMCConnectionError(IBMCError):
 
 # Undo patch function
 function undo_patch {
-    if [ -e $PATH_CONF_INIT_BAK ]
-    then
-        mv -f $PATH_CONF_INIT_BAK $PATH_CONF_INIT
-    fi
-    if [ -e $PATH_COMMON_EXCEPTION_BAK ]
-    then
-        mv -f $PATH_COMMON_EXCEPTION_BAK $PATH_COMMON_EXCEPTION
-    fi
-    if [ -e $PATH_ENTRY_POINTS_BAK ]
-    then
-        mv -f $PATH_ENTRY_POINTS_BAK $PATH_ENTRY_POINTS
-    fi
-    if [ -e $PATH_IRONIC_CONF_BAK ]
-    then
-        mv -f $PATH_IRONIC_CONF_BAK $PATH_IRONIC_CONF
-    if
+    make_copy
 
-    echo "Undo patch done!"
+    # Remove iBMC related configuration and codes...
+    # Code ...
+    local TMP=${CODE_CONF_INIT//\(/\\(}
+    TMP=${TMP//\)/\\)}
+    perl -0777 -i -pe "s/$TMP//g" $PATH_CONF_INIT
+
+    TMP=${CODE_COMMON_EXCEPTION//\(/\\(}
+    TMP=${TMP//\)/\\)}
+    perl -0777 -i -pe "s/$TMP//g" $PATH_COMMON_EXCEPTION
+    # Entry points ...
+    sed -i -r -e "s/^$IBMC_HW//" $PATH_ENTRY_POINTS
+    sed -i -r -e "s/^$IBMC_MGMT//" $PATH_ENTRY_POINTS
+    sed -i -r -e "s/^$IBMC_POWER//" $PATH_ENTRY_POINTS
+    sed -i -r -e "s/^$IBMC_VENDOR//" $PATH_ENTRY_POINTS
+    # Config ...
+    # Ironic don't allow some option value be empty list (empty string).
+    # In case option value become empty list after uninstall, 
+    #+ we leave comma there intentionally
+    sed -i -r -e "s/^($IRONIC_CFG_HW=.*)(ibmc)(.*)/\1\3/" $PATH_IRONIC_CONF
+    sed -i -r -e "s/^($IRONIC_CFG_MGMT=.*)(ibmc)(.*)/\1\3/" $PATH_IRONIC_CONF
+    sed -i -r -e "s/^($IRONIC_CFG_POWER=.*)(ibmc)(.*)/\1\3/" $PATH_IRONIC_CONF
+    sed -i -r -e "s/^($IRONIC_CFG_VENDOR=.*)(ibmc)(.*)/\1\3/" $PATH_IRONIC_CONF
+
+    echo "Uninstall patch done!"
 }
 
 if [ "$IS_PATCH" -eq "0" ]
